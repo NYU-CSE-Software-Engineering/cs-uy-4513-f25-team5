24eb00e584f0736a43a937704f72efc7a95ec214
MoayadAlismail <moayadalismail@gmail.com>
2025-11-06 23:51:18 -0500

    test: add user email presence spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it 'is invalid without an email' do
-      user = User.new(password: 'secret123')
+      user = described_class.new(email: nil)
 
       expect(user).not_to be_valid
-      user.validate
       expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it 'is invalid without a password' do
-      user = User.new(email: 'test@example.com')
-
-      expect(user).not_to be_valid
-      user.validate
-      expect(user.errors[:password]).to include("can't be blank")
-    end
-
-    it 'is invalid with a duplicate email' do
-      User.create!(email: 'duplicate@example.com', password: 'secret123')
-      user = User.new(email: 'duplicate@example.com', password: 'anothersecret')
-
-      expect(user).not_to be_valid
-      user.validate
-      expect(user.errors[:email]).to include('has already been taken')
     end
   end
 end
