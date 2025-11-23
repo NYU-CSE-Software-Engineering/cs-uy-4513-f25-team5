69cc42fde87c81ffad1c4852cd704118bcad5900
MoayadAlismail <moayadalismail@gmail.com>
2025-11-23 12:07:24 -0500

    test: conversation should have many messages

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe Conversation, type: :model do
 
       expect(conversation.participant_one).to eq(user1)
     end
+
+    it 'has many messages' do
+      user1 = User.create!(email: 'user1@example.com', password: 'password123')
+      user2 = User.create!(email: 'user2@example.com', password: 'password123')
+      conversation = Conversation.create!(participant_one_id: user1.id, participant_two_id: user2.id)
+      message = Message.create!(conversation: conversation, user: user1, body: 'Hello')
+
+      expect(conversation.messages).to include(message)
+    end
   end
 end
