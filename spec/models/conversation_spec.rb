90485520e5a2d39c6ecb6335398ffdde32340f90
MoayadAlismail <moayadalismail@gmail.com>
2025-11-23 12:03:27 -0500

    test: add conversation participant association spec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  describe 'validations' do
-    it 'is invalid without both participants' do
-      conversation = described_class.new(participant_one: User.new, participant_two: nil)
+  describe 'associations' do
+    it 'belongs to participant_one as a User' do
+      user1 = User.create!(email: 'user1@example.com', password: 'password123')
+      user2 = User.create!(email: 'user2@example.com', password: 'password123')
+      conversation = Conversation.create!(participant_one_id: user1.id, participant_two_id: user2.id)
 
-      expect(conversation).not_to be_valid
-      expect(conversation.errors[:participant_two]).to include("can't be blank")
+      expect(conversation.participant_one).to eq(user1)
     end
   end
 end
