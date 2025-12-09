b39c977d3857ac0933c76b36b9d13b8b84a75ec1
mAE7777 <sitaoma7@gmail.com>
2025-12-09 18:28:07 -0500

    Add to_params method to SearchHistory

--- app/models/search_history.rb
+++ app/models/search_history.rb
@@ -2,4 +2,13 @@ class SearchHistory < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
+
+  def to_params
+    {
+      city: city,
+      min_price: min_price,
+      max_price: max_price,
+      keywords: keywords
+    }.compact_blank
+  end
 end
