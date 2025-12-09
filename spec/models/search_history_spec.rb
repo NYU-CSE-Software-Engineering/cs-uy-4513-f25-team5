635e8b9500a10bb300f8a0d880daf7ef34c0c98e
mAE7777 <sitaoma7@gmail.com>
2025-12-09 18:19:58 -0500

    Add failing spec for SearchHistory user association

---
+++ spec/models/search_history_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe SearchHistory, type: :model do
+  describe 'associations' do
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+end
